61c7ac07b1bdabc7d8cea4b13e706f87137c5585
yangjp <yangjp@xhsc.com.cn>
2023-07-05 13:55:52 +0800

    fixed stack addr error of MDK.

--- bsp/hc32/ev_hc32f4a2_lqfp176/board/board.h
+++ bsp/hc32/ev_hc32f4a2_lqfp176/board/board.h
@@ -24,8 +24,8 @@ extern "C" {
 #define HC32_SRAM_END                   (0x1FFE0000 + HC32_SRAM_SIZE * 1024)
 
 #ifdef __ARMCC_VERSION
-extern int Image$$RW_IRAM1$$ZI$$Limit;
-#define HEAP_BEGIN                      (&Image$$RW_IRAM1$$ZI$$Limit)
+extern int Image$$RW_IRAM2$$ZI$$Limit;
+#define HEAP_BEGIN                      (&Image$$RW_IRAM2$$ZI$$Limit)
 #elif __ICCARM__
 #pragma section="HEAP"
 #define HEAP_BEGIN                      (__segment_end("HEAP"))
