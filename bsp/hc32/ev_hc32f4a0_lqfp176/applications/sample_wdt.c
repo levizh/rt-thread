ca7f3f93a164b2ba6ed26c7d91b07675267362ad
c_coder <code9527@126.com>
2023-11-09 15:21:10 +0800

    [F4A0] add wdt test sample

---
+++ bsp/hc32/ev_hc32f4a0_lqfp176/applications/sample_wdt.c
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2006-2022, RT-Thread Development Team
+ * Copyright (c) 2022, Xiaohua Semiconductor Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-11-09     CDT          first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <rtdef.h>
+#include "board_config.h"
+#include "drv_wdt.h"
+
+/* macros define */
+#define SAMPLE_WDT_NAME     "wdt"
+#define WDT_FED_CNT         (10)
+
+/* variable define */
+static rt_device_t wdt_dev;
+static char wdt_name[RT_NAME_MAX];
+
+/**
+ * @brief function to feed watchdog at mcu free time
+ */
+static void idle_hook(void)
+{
+    rt_uint32_t wdt_timeout;
+    static rt_uint32_t wdt_feed_cnt = 0;
+
+    /* when wdt fed over 10 times, let wdt timeout and reset system */
+    if (WDT_FED_CNT <= wdt_feed_cnt)
+    {
+        rt_thread_idle_delhook(idle_hook);
+        rt_kprintf("stop feed %s and waiting reboot\n", wdt_name);
+        return;
+    }
+
+    if(RT_EOK != rt_device_control(wdt_dev, RT_DEVICE_CTRL_WDT_KEEPALIVE, RT_NULL))
+    {
+        rt_kprintf("feed %s failed\n", wdt_name);
+        return;
+    }
+    else
+    {
+        ++wdt_feed_cnt;
+        rt_kprintf("[%2u] %s fed\n", wdt_feed_cnt, wdt_name);
+    }
+
+    if(RT_EOK != rt_device_control(wdt_dev, RT_DEVICE_CTRL_WDT_GET_TIMEOUT, &wdt_timeout))
+    {
+        rt_kprintf("ctrl: get timeout %s failed\n", wdt_name);
+        return;
+    }
+    else
+    {
+        rt_kprintf("%s left time is %dms\n", wdt_name, wdt_timeout);
+    }
+}
+
+/**
+ * @brief function to receive command
+ *
+ * @param argc input parameter number
+ * @param argv array to store the input strings
+ */
+static int wdt_sample(int argc, char *argv[])
+{
+    rt_uint32_t wdt_timeout = 1;
+
+    if (argc == 2)
+    {
+        rt_strncpy(wdt_name, argv[1], RT_NAME_MAX);
+    }
+    else
+    {
+        rt_strncpy(wdt_name, SAMPLE_WDT_NAME, RT_NAME_MAX);
+    }
+
+    wdt_dev = rt_device_find(wdt_name);
+    if(!wdt_dev)
+    {
+        rt_kprintf("find %s failed\n", wdt_name);
+        return RT_ERROR;
+    }
+
+    if(RT_EOK != rt_device_init(wdt_dev))
+    {
+        rt_kprintf("initial %s failed\n", wdt_name);
+        return RT_ERROR;
+    }
+
+    rt_kprintf("wdt test begin...\n");
+
+    if(RT_EOK != rt_device_control(wdt_dev, RT_DEVICE_CTRL_WDT_SET_TIMEOUT, &wdt_timeout))
+    {
+        rt_kprintf("ctrl: set timeout %s failed\n", wdt_name);
+        return RT_ERROR;
+    }
+
+    if(RT_EOK != rt_device_control(wdt_dev, RT_DEVICE_CTRL_WDT_START, RT_NULL))
+    {
+        rt_kprintf("ctrl: start %s failed\n", wdt_name);
+        return RT_ERROR;
+    }
+
+    if(RT_EOK != rt_device_control(wdt_dev, RT_DEVICE_CTRL_WDT_STOP, RT_NULL))
+    {
+        rt_kprintf("ctrl: stop %s failed, command is under supporting\n", wdt_name);
+    }
+    else
+    {
+        rt_kprintf("%s stopped\n", wdt_name);
+    }
+
+    rt_thread_idle_sethook(idle_hook);
+
+    return RT_EOK;
+}
+
+/* export to function msh command list */
+MSH_CMD_EXPORT(wdt_sample, wdt device sample);
