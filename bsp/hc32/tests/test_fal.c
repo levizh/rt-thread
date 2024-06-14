4811a331977f0c6f2e759dd5d20a26dfcf6cc6e8
JamieTx <yangjp24@126.com>
2024-06-14 16:54:47 +0800

    [F448][F472] Modify efm test examples.

--- bsp/hc32/tests/test_fal.c
+++ bsp/hc32/tests/test_fal.c
@@ -1,5 +1,4 @@
 /*
- * Copyright (c) 2006-2022, RT-Thread Development Team
  * Copyright (c) 2022, Xiaohua Semiconductor Co., Ltd.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -8,7 +7,6 @@
  * Date           Author       Notes
  * 2023-11-10     CDT          first version
  */
-
 
 /*
  * 程序清单： FAL使用例程
@@ -19,47 +17,53 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 
-#ifdef RT_USING_FAL
+#if defined(RT_USING_FAL) && defined(BSP_USING_ON_CHIP_FLASH)
 
-#include "board_config.h"
+#include "board.h"
 #include <fal.h>
 
-#define Partition_Name   "app"
-#define Buff_Size        1024
-#define TEST_OFF         0x50000
-#define TEST_CNT         32
+#define FAL_PART_NAME                   "app"
+#define TEST_BUF_SIZE                   1024UL
+#define TEST_RW_CNT                     32UL
+#define TEST_RW_START_ADDR              HC32_FLASH_END_ADDRESS - (TEST_BUF_SIZE * TEST_RW_CNT)
+
+
+static uint8_t write_buffer[TEST_BUF_SIZE] = {0};
+static uint8_t read_buffer[TEST_BUF_SIZE] = {0};
+
 
 static int fal_sample(int argc, char **argv)
 {
     const struct fal_partition *param;
     int ret;
-    uint8_t write_buffer[Buff_Size] = {0};
-    uint8_t read_buffer[Buff_Size] = {0};
     uint32_t Address;
     uint8_t errFlag = 0;
 
-    fal_init(); //抽象层初始化
+    fal_init();     //抽象层初始化
     /* Set write buffer, clear read buffer */
-    for (uint16_t index = 0; index < Buff_Size; index++)
+    for (uint32_t index = 0; index < TEST_BUF_SIZE; index++)
     {
         write_buffer[index] = index;
-        read_buffer[index] = 0;
     }
-
-    param = fal_partition_find(Partition_Name);
-
+    param = fal_partition_find(FAL_PART_NAME);
     if (param == RT_NULL)
     {
         rt_kprintf("not find partition app!\r\n");
         return -1;
     }
-    for (int j = 0; j < TEST_CNT; j++)
+    for (int j = 0; j < TEST_RW_CNT; j++)
     {
         errFlag = 0;
-        Address = TEST_OFF + j * Buff_Size;
-        rt_kprintf("........test %d address %x........\r\n", j + 1, Address);
+        Address = TEST_RW_START_ADDR + j * TEST_BUF_SIZE;
+        rt_kprintf("........test %d address 0x%08x........\r\n", j + 1, Address);
         /* erase process */
-        ret = fal_partition_erase(param, Address, Buff_Size);
+
+        if (j == 31)
+        {
+            rt_kprintf(".......");
+        }
+
+        ret = fal_partition_erase(param, Address, TEST_BUF_SIZE);
         if (ret >= 0)
         {
             rt_kprintf("Erase succeeded!\r\n");
@@ -69,9 +73,8 @@ static int fal_sample(int argc, char **argv)
             rt_kprintf("Erase failed!\r\n");
             return ret;
         }
-
         /* write process */
-        ret = fal_partition_write(param, Address, write_buffer, sizeof(write_buffer));
+        ret = fal_partition_write(param, Address, write_buffer, TEST_BUF_SIZE);
         if (ret >= 0)
         {
             rt_kprintf("Write succeeded!\r\n");
@@ -81,8 +84,12 @@ static int fal_sample(int argc, char **argv)
             rt_kprintf("Write failed!\r\n");
             return ret;
         }
-        /* read out and compare */
-        ret = fal_partition_read(param, Address, read_buffer, Buff_Size);
+        /* read process */
+        for (uint32_t index = 0; index < TEST_BUF_SIZE; index++)
+        {
+            read_buffer[index] = 0;
+        }
+        ret = fal_partition_read(param, Address, read_buffer, TEST_BUF_SIZE);
         if (ret >= 0)
         {
             rt_kprintf("Read succeeded!\r\n");
@@ -92,13 +99,18 @@ static int fal_sample(int argc, char **argv)
             rt_kprintf("Read failed!\r\n");
             return ret;
         }
-
-        for (int i = 0; i < Buff_Size; i++)
+        /* compare process */
+        for (int i = 0; i < TEST_BUF_SIZE; i++)
         {
+#if defined(HC32F460)
+            if ((j == (TEST_RW_CNT - 1)) && (i >= (TEST_BUF_SIZE - 32)) ?
+                    (read_buffer[i] != 0xFF) : (read_buffer[i] != write_buffer[i]))
+#else
             if (read_buffer[i] != write_buffer[i])
+#endif
             {
                 rt_kprintf("Data verification failed:\r\n");
-                rt_kprintf("NUM: %d  Write: %x    Read: %x \r\n", i, write_buffer[i], read_buffer[i]);
+                rt_kprintf("NUM: %d  Write: %x Read: %x \r\n", i, write_buffer[i], read_buffer[i]);
                 errFlag = 1;
                 ret = -1;
             }
@@ -114,4 +126,5 @@ static int fal_sample(int argc, char **argv)
 
 /* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(fal_sample, fal sample);
+
 #endif
