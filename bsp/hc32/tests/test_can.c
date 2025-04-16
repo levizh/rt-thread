add00649cdd084dbe23f72b2cbe64af47fde8be4
hexiao <hexiao@xhsc.com.cn>
2025-04-16 16:46:38 +0800

    [bsp][hc32][HC32F4A8][drv] support canfd

--- bsp/hc32/tests/test_can.c
+++ bsp/hc32/tests/test_can.c
@@ -395,9 +395,9 @@ int can_sample(int argc, char **argv)
         res = rt_device_control(can_dev, RT_CAN_CMD_SET_BAUD_FD, (void *)MCANFD_DATA_BAUD_4M);
 #else
         res = rt_device_control(can_dev, RT_CAN_CMD_SET_BAUD_FD, (void *)CANFD_DATA_BAUD_4M);
-#endif
 #if defined (HC32F4A8)
         res = rt_device_control(can_dev, RT_CAN_CMD_SET_CANFD, (void *)ENABLE);
+#endif
 #endif
         RT_ASSERT(res == RT_EOK);
     }
