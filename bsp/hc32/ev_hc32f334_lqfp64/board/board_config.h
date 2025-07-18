2cafa4a874cf1a77256a30b6787d145dbfba8476
Leeto1970 <821522988@qq.com>
2025-07-18 18:35:38 +0800

    [F334] Add rtc/hwtimer support.

--- bsp/hc32/ev_hc32f334_lqfp64/board/board_config.h
+++ bsp/hc32/ev_hc32f334_lqfp64/board/board_config.h
@@ -131,8 +131,7 @@
 #if defined(BSP_USING_RTC) || defined(RT_USING_PM)
     #if defined(BSP_RTC_USING_XTAL32) || defined(RT_USING_PM)
         #define XTAL32_PORT                     (GPIO_PORT_C)
-        #define XTAL32_IN_PIN                   (GPIO_PIN_14)
-        #define XTAL32_OUT_PIN                  (GPIO_PIN_15)
+        #define XTAL32_PIN                      (GPIO_PIN_14 | GPIO_PIN_15)
     #endif
 #endif
 
