3c660452b4bd430896a78930fef5df6539e5b820
liangchuan <liangchuan@xhsc.com.cn>
2025-08-11 15:00:09 +0800

    [HC32]Add cherryusb test code.

---
+++ bsp/hc32/tests/test_cherryusb.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022-2024, Xiaohua Semiconductor Co., Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2025-08-08     CDT          first version
+ */
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+/* MDK: 使用 Version 5 Compiler，勾选 Option --> C/C+ --> GNU extensions
+   CherryUSB 与 legacy USB 组件不可同时使用
+*/
+
+#if defined(RT_CHERRYUSB_HOST) && defined(RT_CHERRYUSB_DEVICE)
+    #if defined(HC32F4A0) || defined(HC32F4A8)
+        #define TEST_USBH_CORE_BASE   (CM_USBFS_BASE)
+        #define TEST_USBD_CORE_BASE   (CM_USBHS_BASE)
+    #else
+        #error "Only one USB role can be selected"
+    #endif
+#else
+    #if defined(RT_CHERRYUSB_HOST)
+        #define TEST_USBH_CORE_BASE   (CM_USBFS_BASE)
+    #elif defined(RT_CHERRYUSB_DEVICE)
+        #define TEST_USBD_CORE_BASE   (CM_USBFS_BASE)
+    #endif
+#endif
+
+#if defined(RT_CHERRYUSB_HOST)
+#include "usbh_core.h"
+#if defined(RT_CHERRYUSB_HOST_CDC_ECM) || defined(RT_CHERRYUSB_HOST_CDC_RNDIS)
+/*  ECM/RNDIS 测试
+msh />ipconfig
+    network interface: u0 (Default)
+    MTU: 1500
+    MAC: 4e 61 e9 06 9b ff
+    FLAGS: UP LINK_UP ETHARP BROADCAST IGMP
+    ip address: 192.168.225.29
+    gw address: 192.168.225.1
+    net mask  : 255.255.255.0
+    dns server #0: 108.7.254.31
+    dns server #1: 112.7.254.31
+msh />ping www.baidu.com
+    60 bytes from 183.2.172.177 icmp_seq=0 ttl=51 time=149 ms
+    60 bytes from 183.2.172.177 icmp_seq=1 ttl=51 time=69 ms
+    60 bytes from 183.2.172.177 icmp_seq=2 ttl=51 time=51 ms
+    60 bytes from 183.2.172.177 icmp_seq=3 ttl=51 time=52 ms
+*/
+static int cherryusb_host_init(void)
+{
+    usbh_initialize(0, TEST_USBH_CORE_BASE);
+    return 0;
+}
+INIT_APP_EXPORT(cherryusb_host_init);
+
+void ipconfig(void)
+{
+    extern void list_if(void);
+    list_if();
+}
+MSH_CMD_EXPORT(ipconfig, list network interface information);
+
+#endif
+#endif
+
+#if defined(RT_CHERRYUSB_DEVICE)
+#if defined(RT_CHERRYUSB_DEVICE_TEMPLATE_CDC_ACM)
+
+static int  cherryusb_device_cdc_acm_init(void)
+{
+    extern void cdc_acm_init(uint8_t busid, uint32_t reg_base);
+    cdc_acm_init(0, TEST_USBD_CORE_BASE);
+    return 0;
+}
+INIT_APP_EXPORT(cherryusb_device_cdc_acm_init);
+
+static void cherryusb_cdc_send(int argc, char **argv)
+{
+    extern void cdc_acm_data_send_with_dtr_test(uint8_t busid);
+    cdc_acm_data_send_with_dtr_test(0);
+}
+MSH_CMD_EXPORT(cherryusb_cdc_send, cdc acm data send with dtr test);
+
+#endif
+#endif
